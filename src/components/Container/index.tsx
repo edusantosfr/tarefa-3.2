fedf67b1cdf90f793b28cfbcd3bb8a965a4d57e5
edusantosfr <70604190+edusantosfr@users.noreply.github.com>
2025-02-12 20:17:41 -0300

    last commit

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 
 type Filme = {
@@ -13,25 +13,63 @@ type Props = {
 }
 
 export const FilmsTable: React.FC<Props> = ({ film }) => {
+    const [checked, setchecked] = useState<number[]>([]);
+    const [openedModal, setOpenedModal] = useState<boolean>(false);
+
+    const handleCheckboxChange = (id: number) => {
+        if (checked.includes(id)) {
+            setchecked(checked.filter(element => element !== id));
+        } else {
+            setchecked([...checked, id]);
+        }
+    };
+
+    useEffect(() => {
+        if (checked.length > 0) {
+            setOpenedModal(true);
+        }
+    }, [checked]);
+
+    const closeModal = () => setOpenedModal(false);
+
     return (
-        <table border={1} className="table">
-            <thead className="tableHead">
-                <tr>
-                    <th><b><p>Banner</p></b></th>
-                    <th><b><p>Nome</p></b></th>
-                    <th><b><p>Gênero</p></b></th>
-                </tr>
-            </thead>
-            <tbody className="tableBody">
-                {film.length > 0 && film.map((element) => (
-                    <tr key={element.id} className="tableItems">
-                        <td><img src={element.imagem} alt="banner de filme" /></td>
-                        <td><h3>{element.nome}</h3></td>
-                        <td><p>{element.genero}</p></td>
+        <div>
+            <table border={1} className="table">
+                <thead className="tableHead">
+                    <tr>
+                        <th><b><p>Banner</p></b></th>
+                        <th><b><p>Nome</p></b></th>
+                        <th><b><p>Gênero</p></b></th>
+                        <th><b><p>Visto</p></b></th>
                     </tr>
-                ))
-                }
-            </tbody>
-        </table>
+                </thead>
+                <tbody className="tableBody">
+                    {film.length > 0 && film.map((element) => (
+                        <tr key={element.id} className="tableItems">
+                            <td><img src={element.imagem} alt="banner de filme" /></td>
+                            <td><h3>{element.nome}</h3></td>
+                            <td><p>{element.genero}</p></td>
+                            <td><input type="checkbox" checked={checked.includes(element.id)} onChange={() => {
+                                handleCheckboxChange(element.id)
+                            }} /></td>
+                        </tr>
+                    ))
+                    }
+                </tbody>
+            </table>
+            {openedModal && (
+                <div className="modalStyle">
+                    <div className="modalContentStyle">
+                        <h3>Filmes Vistos:</h3>
+                        {checked.length > 0 && checked.map((id) => {
+                            const selectedMovie = film.find((element) => element.id === id);
+                            return <li key={id}>{selectedMovie?.nome}</li>;
+                        })
+                        }
+                        <button className="closeBtn" onClick={closeModal}>Fechar</button>
+                    </div>
+                </div>
+            )}
+        </div>
     );
-};
+}
